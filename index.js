6c387e58a14d29fa1aeb7baed8780428b469174f
Mark Gardner <themarkgardner@gmail.com>
2022-10-21 22:19:09 -0500

    Populated initial questions and basic logic for inquirer questions

--- index.js
+++ index.js
@@ -1,9 +1,120 @@
-
+const inquirer = require('inquirer');
+const fs = require('fs')
+const Employee = require('./lib/Employee')
 const Manager = require('./lib/Manager')
-// const Engineer = require('./lib/Engineer')
-// const Intern = require('./lib/Intern')
+const Engineer = require('./lib/Engineer')
+const Intern = require('./lib/Intern');
 
-const employee = new Manager('Jake', 4, 'jake@email');
 
-console.log(employee);
+//set up array of all the employees
+let empArray = [];
 
+//set up array of questions for each role in the company
+const mgrQuest = [
+    {
+        type: 'input',
+        message: `What is the Team Manager's name?`,
+        name: 'name',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Team Manager's employee ID number?`,
+        name: 'id',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Team Manager's e-mail address?`,
+        name: 'email',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Team Manager's office number?`,
+        name: 'officeNumber',
+    }, 
+    {
+        type: 'list',
+        message: `Would you like to any more Team Members?`,
+        choices: ['Engineer','Intern','Finish'],
+        name: 'addMore',
+    },
+]
+
+const engQuest = [
+    {
+        type: 'input',
+        message: `What is the Engineer's name?`,
+        name: 'name',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Engineer's employee ID number?`,
+        name: 'id',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Engineer's e-mail address?`,
+        name: 'email',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Engineer's GitHub username?`,
+        name: 'gitHub',
+    }, 
+    {
+        type: 'list',
+        message: `Would you like to any more Team Members?`,
+        choices: ['Engineer','Intern','Finish'],
+        name: 'addMore',
+    },
+]
+
+const intQuest = [
+    {
+        type: 'input',
+        message: `What is the Intern's name?`,
+        name: 'name',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Intern's employee ID number?`,
+        name: 'id',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Intern's e-mail address?`,
+        name: 'email',
+    }, 
+    {
+        type: 'input',
+        message: `What is the Intern's school?`,
+        name: 'school',
+    }, 
+    {
+        type: 'list',
+        message: `Would you like to any more Team Members?`,
+        choices: ['Engineer','Intern','Finish'],
+        name: 'addMore',
+    },
+]
+
+function init(questArray) {
+    inquirer.prompt(questArray).then((response) => {
+        empArray.push(response);
+        if (response.addMore === 'Engineer') {          
+            init(engQuest);
+            return;
+        } else if (response.addMore === 'Intern') {
+            init(intQuest);
+            return;
+        } else if (response.addMore === 'Finish') {
+            //buildHTML
+            return;
+        } else {
+            console.log('There was an error in your selection. Reload the page and try again')
+        }
+        })
+    };
+
+
+//function to initialize app
+init(mgrQuest);
