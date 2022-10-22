a35b8379394b7c371d679245f54dcc7bb67d10b8
Mark Gardner <themarkgardner@gmail.com>
2022-10-21 21:31:27 -0500

    Initial deploy with starter code

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const Manager = require('./lib/Manager')
+// const Engineer = require('./lib/Engineer')
+// const Intern = require('./lib/Intern')
+
+const employee = new Manager('Jake', 4, 'jake@email');
+
+console.log(employee);
+
